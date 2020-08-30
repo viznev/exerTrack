73294aea9c69795ad1b3f3d165a305bdab240fbf
viznev <viznev@users.noreply.github.com>
2020-08-30 13:52:03 -0500

    added edit-exercise component

---
+++ mern-exercise-tracker/src/components/edit-exercise.component.js
@@ -0,0 +1,11 @@
+import React, { component, Component } from 'react';
+
+export default class EditExercise extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Edit Exercise component!</p>
+            </div>
+        )
+    }
+}
