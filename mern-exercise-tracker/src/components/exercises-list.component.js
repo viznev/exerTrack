41fa468143a5bf616d7320068276fb9f825f32a3
viznev <viznev@users.noreply.github.com>
2020-08-30 13:52:14 -0500

    added exercise-list component

---
+++ mern-exercise-tracker/src/components/exercises-list.component.js
@@ -0,0 +1,11 @@
+import React, { component, Component } from 'react';
+
+export default class ExercisesList extends Component {
+    render() {
+        return (
+            <div>
+                <p>You are on the Exercises list component!</p>
+            </div>
+        )
+    }
+}
